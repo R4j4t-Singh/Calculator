bcd5e7ac3e9f0810d67fe1e1e844ef9d5a4dbf0b
yorajatsingh <76911341+yorajatsingh@users.noreply.github.com>
2021-02-02 19:53:46 +0530

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,d;
+    char c;
+    while(a)
+    {
+    cout<<"CALCULATOR \n";
+    cin>>a>>c>>b;
+    switch(c)
+{
+    case '+': d = a + b;
+              cout<<d; 
+             break;
+    case '-': d = a - b;
+              cout<<d; 
+             break;
+    case '*': d = a * b;
+              cout<<d; 
+             break;
+    case '/':if(b==0)
+              {
+                  cout<<"The number cannot be divided by zero \n";
+                  break;
+              } 
+             d = a / b; 
+             cout<<d;  
+             break;        
+}
+
+    cout<<"  \n\n\n\n";    
+    }
+
+
+  return 0;  
+}
