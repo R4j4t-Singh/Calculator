bee33433d9bacc9b509a9c6fc99603427f1d6f4f
Hrishubh Bhandari <bhandarihrishubh@gmail.com>
2021-02-06 13:48:00 +0530

    Calculator Updated

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,37 +1,38 @@
-#include<iostream>
+
+#include <bits/stdc++.h>
+
+#define int            long long
+#define float          double
+#define vi             vector<int>
+#define FIO            ios_base::sync_with_stdio(false); cin.tie(NULL);
+
 using namespace std;
-int main()
-{
-    float a,b,d;
+
+int32_t main(){
+    cout<<"Please enter the two numbers on which the operation is to be performed:  ";
+    float a,b;
+    cin>>a>>b;
+    cout<<"Please enter the operator to be used in calculation: ";
     char c;
-    while(a)
+    cin>>c;
+    switch (c)
     {
-    cout<<"CALCULATOR \n";
-    cin>>a>>c>>b;
-    switch(c)
-{
-    case '+': d = a + b;
-              cout<<d; 
-             break;
-    case '-': d = a - b;
-              cout<<d; 
-             break;
-    case '*': d = a * b;
-              cout<<d; 
-             break;
-    case '/':if(b==0)
-              {
-                  cout<<"The number cannot be divided by zero \n";
-                  break;
-              } 
-             d = a / b; 
-             cout<<d;  
-             break;        
-}
+    case '+':
+        cout<<"The sum is equal to: "<<a+b;
 
-    cout<<"\n\n\n\n";    
+        break;
+    case '-':
+        cout<<"The difference of the two numbers is:    "<<(a>b)?a-b:b-a;
+        break;
+    case '*':
+        cout<<"The product of the two numbers is:   "<<a*b;
+        break;
+    case '/':
+        cout<<"The division of the two numbers gives the result:    "<<a/b;
+        break;
+    default:
+        cout<<"Wrong input operator!!";
+        break;
     }
-
-
-  return 0;  
+    return 0;
 }
